356584a8da92e739a24e1e26bbb137c62cf6ec89
Kartik Singh Chuphal <64568294+kartik077-cr7@users.noreply.github.com>
2021-09-15 11:24:29 +0530

    fixed react duplicate

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect,useState} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import useStyles from './styles';
+const alanKey = 'a5f32de9ffe131c4fff7ec942bfbe7ad2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 
 function App() {
+  
+  useEffect(() => {
+     
+    alanBtn({
+       key: alanKey,
+       onCommand:({command,articles,number}) => {
+             if(command === 'testing'){
+              alanBtn().playText('Testing Successfull...');
+             }         
+       }
+    })
+  },[])
+
+  const classes = useStyles();
   return (
-    <div className="App">
-       <h1>JUST CHECKING</h1>
-    </div>
+     <div>
+       HELLO WORLD
+     </div>
   );
 }
 
