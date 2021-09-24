047bda99205c146527e92b1ff5f2727a9f367415
Harshal Gadhe <51368042+harshalgadhe@users.noreply.github.com>
2021-09-24 23:43:34 +0530

    Update views.py

--- DjangoBackend/FlipBackend/Grid/views.py
+++ DjangoBackend/FlipBackend/Grid/views.py
@@ -6,6 +6,18 @@ from Grid.models import Review
 from Weights.predict import prediction_final
 import os 
 import pickle 
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import model_selection, naive_bayes, svm
+from sklearn.metrics import accuracy_score
 
 path = os.getcwd()+'/Weights'
 
