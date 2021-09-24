3b61684163b17fcf3d212c20da20594795aebcd6
Harshal Gadhe <51368042+harshalgadhe@users.noreply.github.com>
2021-09-24 20:33:28 +0530

    Update views.py

--- DjangoBackend/FlipBackend/Grid/views.py
+++ DjangoBackend/FlipBackend/Grid/views.py
@@ -3,11 +3,11 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from Grid.models import Review
-from Weights import prediction_final
+from Weights.prediction import prediction_final
 import os 
-path = os.getcwd()+'/FlipBackend/Weights'
-
 import pickle 
+
+path = os.getcwd()+'/FlipBackend/Weights'
 
 svm0 = pickle.load(open(path+'/finalized_model0.sav', 'rb'))
 svm1= pickle.load(open(path+'/finalized_model1.sav', 'rb'))
@@ -23,7 +23,8 @@ def storeReview(request):
     userId = data['UserId']
     customerName = data['CustomerName']
     order_No = data["OrderNo"]
-    review = Review(customer_name = customerName, order_no = order_No, review_text = feedback,categories_detected = "1 2")
+    category = prediction_final(feedback)
+    review = Review(customer_name = customerName, order_no = order_No, review_text = feedback,categories_detected = category)
     print("review is ",review)
     review.save()
     return JsonResponse({'valid':1})
